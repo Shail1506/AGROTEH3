da9dbf43508348e19a5a58f3113e7e10b75a2dcb
Shail1506 <patel.shail98@gmail.com>
2019-09-22 00:33:10 +0530

    Initial commit

---
+++ app/src/main/java/com/rsrfarming/agroteh/selectlanguage.java
@@ -0,0 +1,14 @@
+package com.rsrfarming.agroteh;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class selectlanguage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_selectlanguage);
+    }
+}
